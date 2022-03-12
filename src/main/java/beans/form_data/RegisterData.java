8ed886956abbf7e8587ba2d033683e1237262ec8
HoangTien339 <tiennh21@fpt.edu.vn>
2022-03-12 16:31:08 +0700

    Servlet LifeCycle

---
+++ src/main/java/beans/form_data/RegisterData.java
@@ -0,0 +1,54 @@
+package beans.form_data;
+
+public class RegisterData {
+	private String hoTen;
+	private String diaChi;
+	private String sdt;
+	private String email;
+	private String password;
+	private int gioiTinh;
+	private int loaiKH;
+
+	public String getHoTen() {
+		return hoTen;
+	}
+	public void setHoTen(String hoTen) {
+		this.hoTen = hoTen;
+	}
+	public String getDiaChi() {
+		return diaChi;
+	}
+	public void setDiaChi(String diaChi) {
+		this.diaChi = diaChi;
+	}
+	public String getSdt() {
+		return sdt;
+	}
+	public void setSdt(String sdt) {
+		this.sdt = sdt;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getGioiTinh() {
+		return gioiTinh;
+	}
+	public void setGioiTinh(int gioiTinh) {
+		this.gioiTinh = gioiTinh;
+	}
+	public int getLoaiKH() {
+		return loaiKH;
+	}
+	public void setLoaiKH(int loaiKH) {
+		this.loaiKH = loaiKH;
+	}
+}
--- src/main/java/controllers/HelloServlet.java
+++ src/main/java/controllers/HelloServlet.java
@@ -1,6 +1,8 @@
 package controllers;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/HelloServlet")
 public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private ArrayList<Integer> list;
        
     public HelloServlet() {
         super();
@@ -31,5 +34,26 @@ public class HelloServlet extends HttpServlet {
 		HttpServletResponse response
 	) throws ServletException, IOException {
 		doGet(request, response);
+	}
+
+	public void init() {
+		System.out.println("Init...");
+		
+		this.list = new ArrayList<Integer>();
+	}
+	
+	protected void service(
+		HttpServletRequest request,
+		HttpServletResponse response
+	) throws ServletException, IOException {
+		this.list.add(1);
+		this.list.add(2);
+		this.list.add(3);
+		System.out.println("Service: " + this.list.size());
+		super.service(request, response);
+	}
+	
+	public void destroy() {
+		System.out.println("Destroy...");
 	}
 }
